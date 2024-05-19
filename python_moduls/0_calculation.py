850b0069e436161e794d9abd25008270c1800dfc
GradPolina <Gradinar_polina@yahoo.com>
2024-05-18 21:04:01 -0500

    Completed task # 0 by 2.2

---
+++ python_moduls/0_calculation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__=="__main__":
+    import calculation
+    print(calculation.add(4,2))
+    print(calculation.sub(4,2))
+    print(calculation.mul(4,2))
+    print(calculation.div(4,2))
