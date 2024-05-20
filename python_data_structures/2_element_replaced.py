af6dc3be6672c76cb6ba647f63941dba337e914b
GradPolina <Gradinar_polina@yahoo.com>
2024-05-20 14:09:11 -0500

    Need to fix, doesn't match to the task conditions in the part print None

---
+++ python_data_structures/2_element_replaced.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+list_ = [5, 4, 3, 2, 1]
+index = 1
+element_to_replace = 5
+def element_replaced(list_1, ind, elem_to_replace):
+    list_1[ind] = element_to_replace
+
+
+element_replaced(list_,index,element_to_replace)
+print(list_)
