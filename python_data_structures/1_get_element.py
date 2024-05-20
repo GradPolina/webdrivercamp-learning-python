a15a796730e37a3247249df2b6d2961356e4d00f
GradPolina <Gradinar_polina@yahoo.com>
2024-05-20 14:10:23 -0500

    Need to fix, doesn't match to the task conditions in the part print None

--- python_data_structures/1_get_element.py
+++ python_data_structures/1_get_element.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 list_ = [5, 4, 3, 2, 1]
-index = -1
+index = -5
 def get_element(list_1, ind):
-    if ind >= 0 and ind <= len(list_1):
-        print("The element retrived is", list_1[ind])
-    elif ind < 0 or ind > len(list_1):
-        return None
-get_element(list_, index)    
-
-
-
-
+    print("The element retrived is", list_1[ind])
+get_element(list_, index)
