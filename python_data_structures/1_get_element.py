d4f7b8a791599795786f888c60de20e4efddb63d
GradPolina <Gradinar_polina@yahoo.com>
2024-05-21 09:16:19 -0500

    completed conditional on the meeting: ind <0 or ind out of range

--- python_data_structures/1_get_element.py
+++ python_data_structures/1_get_element.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 list_ = [5, 4, 3, 2, 1]
-index = -5
+index = 0
+
 def get_element(list_1, ind):
-    print("The element retrived is", list_1[ind])
+    if ind < 0 or ind > len(list_1)-1:
+        print("None")
+    else:    
+        print("The element retrived is", list_1[ind])
+
+
 get_element(list_, index)
--- python_data_structures/2_element_replaced.py
+++ python_data_structures/2_element_replaced.py
@@ -2,8 +2,12 @@
 list_ = [5, 4, 3, 2, 1]
 index = 1
 element_to_replace = 5
+
 def element_replaced(list_1, ind, elem_to_replace):
-    list_1[ind] = element_to_replace
+    if ind < 0 or ind > len(list_1)-1:
+        print("None")
+    else:
+        list_1[ind] = element_to_replace
 
 
 element_replaced(list_,index,element_to_replace)
