b0c01ef59cd2d69ba8db3ef82b60244ec006dace
GradPolina <Gradinar_polina@yahoo.com>
2024-05-29 19:54:38 -0500

    Fixed code

--- python_errors_exceptions/4_divide_list_safe.py
+++ python_errors_exceptions/4_divide_list_safe.py
@@ -4,31 +4,19 @@ def divide_list_safe(list_1, list_2, list_len):
     list_result = []
     for i in range(list_len):
         try: 
-            if i >= len(list_1) or i >= len(list_2):
-                print("out of range")
-                list_result.append(0)
-                continue
-            else:
-                list_result.append(list_1[i]/list_2[j])
-            try:       
-                res=list_1[i]/list_2[i]
-             except TypeError:
-                 print("wrong type")
-                 list_result.append(0)
-                 continue
-
-         except ZeroDivisionError:
-               print("Division by 0")
-               list_result.append(0)
+            result = (list_1[i]/list_2[j])
+        except ZeroDivisionError:
+            print("Division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+        except TypeError:
+            print("wrong type")
+            result = 0
          finally:
-               pass
+            new_list.append(result)
 
-         return list_sesult
-
-  
-
-
-if __name__ == "__main__":
+  if __name__ == "__main__":
     list_1 = [9, 2, 6]
     list_2 = [3, 2, 2]
     res = divide_list_safe(list_1, list_2, max(len(list_1), len(list_2)))
